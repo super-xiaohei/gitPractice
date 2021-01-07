69d2aeb8aa029ab42c578927f0b077469a4e21ba
郑义桥 <1312890342@qq.com>
2021-01-07 18:28:50 +0800

    git的分支练习

---
+++ src/main/java/net/suncaper/springbootmybatisx/SpringbootmybatisxApplication.java
@@ -0,0 +1,13 @@
+package net.suncaper.springbootmybatisx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmybatisxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmybatisxApplication.class, args);
+    }
+
+}
